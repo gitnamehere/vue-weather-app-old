f69494b83d00853ae430119caea55618bd78f972
gitnamehere <105529595+gitnamehere@users.noreply.github.com>
2024-09-21 19:21:20 -0400

    prettier formatting

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { storeToRefs } from 'pinia';
-import { useRoute } from 'vue-router';
+import { onMounted, ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useRoute } from "vue-router";
 
-import { useWeatherStore } from '@/stores/weather';
-import { TemperatureUnits } from '@/utils/constants';
+import { useWeatherStore } from "@/stores/weather";
+import { TemperatureUnits } from "@/utils/constants";
 
-import Searchbar from '@/components/LocationSearchbar.vue';
-import CurrentWeather from '@/components/weather/CurrentWeather.vue';
-import DailyWeather from '@/components/weather/DailyWeather.vue';
-import HourlyWeather from '@/components/weather/HourlyWeather.vue';
-import WeatherGrid from '@/components/weather/WeatherGrid.vue';
+import Searchbar from "@/components/LocationSearchbar.vue";
+import CurrentWeather from "@/components/weather/CurrentWeather.vue";
+import DailyWeather from "@/components/weather/DailyWeather.vue";
+import HourlyWeather from "@/components/weather/HourlyWeather.vue";
+import WeatherGrid from "@/components/weather/WeatherGrid.vue";
 
 const weatherStore = useWeatherStore();
 const { error, temperatureUnit, weather } = storeToRefs(weatherStore);
@@ -32,14 +32,11 @@ onMounted(() => {
 
     getRouteFromParams();
     console.log(window.innerWidth);
-})
+});
 </script>
 
 <template>
-    <div
-        class="weather"
-        :class="weather?.current?.is_day ? 'weather--day' : 'weather--night'"
-    >
+    <div class="weather" :class="weather?.current?.is_day ? 'weather--day' : 'weather--night'">
         <div
             class="weather__top-bar"
             :class="{ 'weather__top-bar--day': weather?.current?.is_day }"
@@ -48,38 +45,23 @@ onMounted(() => {
                 <h1>A Vue Weather App</h1>
             </a>
             <Searchbar class="weather__search-bar" />
-            <button
-                class="weather__menu-button"
-                @click="weatherStore.toggleTemperatureUnit"
-            >
-                {{ temperatureUnit === TemperatureUnits.FAHRENHEIT ? 'F' : 'C' }}
+            <button class="weather__menu-button" @click="weatherStore.toggleTemperatureUnit">
+                {{ temperatureUnit === TemperatureUnits.FAHRENHEIT ? "F" : "C" }}
             </button>
         </div>
-        <div
-            v-if="weather?.current"
-            class="weather__container"
-        >
+        <div v-if="weather?.current" class="weather__container">
             <CurrentWeather />
             <HourlyWeather />
             <WeatherGrid />
             <DailyWeather />
         </div>
-        <div
-            v-else
-            class="weather__container"
-        >
+        <div v-else class="weather__container">
             <template v-if="error">
                 <h1>Cannot fetch weather data</h1>
-                <p>
-                    The location you searched for doesn't exist, or an error occured
-                </p>
+                <p>The location you searched for doesn't exist, or an error occured</p>
             </template>
-            <h1 v-else-if="route.params.location">
-                Loading...
-            </h1>
-            <h1 v-else>
-                Enter in a location to get weather data
-            </h1>
+            <h1 v-else-if="route.params.location">Loading...</h1>
+            <h1 v-else>Enter in a location to get weather data</h1>
         </div>
         <footer class="weather__footer">
             <p>This is the weather page. (under development)</p>
@@ -91,112 +73,111 @@ onMounted(() => {
 </template>
 
 <style scoped lang="scss">
-    .weather {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: 100%;
-        width: calc(100vw - (100vw - 100%)); // subtract viewport by scrollbar width
-        min-height: 100vh;
+.weather {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+    width: calc(100vw - (100vw - 100%)); // subtract viewport by scrollbar width
+    min-height: 100vh;
 
-        padding: 80px 0 16px;
+    padding: 80px 0 16px;
 
-        color: #f8f8f8;
-        text-shadow: 0px 2px 8px #0004;
+    color: #f8f8f8;
+    text-shadow: 0px 2px 8px #0004;
 
-        animation: fadein 0.5s;
-        transition: background-color 0.25s ease;
+    animation: fadein 0.5s;
+    transition: background-color 0.25s ease;
+
+    @media (min-width: 768px) {
+        padding-top: 96px;
+    }
+
+    &__search-bar {
+        width: 100%;
 
         @media (min-width: 768px) {
-            padding-top: 96px;
-        }
-
-        &__search-bar {
-            width: 100%;
-
-            @media (min-width: 768px) {
-                flex-grow: 1;
-                width: unset;
-                margin-left: 16px;
-            }
-        }
-
-
-        &__top-bar {
-            display: flex;
-            position: fixed;
-            inset: 0;
-            flex-direction: row;
-            align-items: center;
-
-            margin-bottom: 2rem;
-            height: 72px;
-            padding: 8px 64px;
-
-            background: linear-gradient(#1d104b, transparent);
-
-            animation: all 0.5s ease;
-
-            &--day {
-                background: linear-gradient(#89c6ff, #2885dd, transparent);
-            }
-
-            @media (max-width: 767px) {
-                padding: 16px;
-
-                h1 {
-                    display: none;
-                }
-            }
-
-            h1 {
-                font-weight: 500;
-                font-size: 24px;
-                color: #f8f8f8;
-            }
-        }
-
-        &__menu-button {
+            flex-grow: 1;
+            width: unset;
             margin-left: 16px;
-
-            height: 40px;
-            width: 40px;
-            border-radius: 16px;
-            border: none;
         }
+    }
 
-        &__container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: start;
-            width: 100%;
+    &__top-bar {
+        display: flex;
+        position: fixed;
+        inset: 0;
+        flex-direction: row;
+        align-items: center;
 
-            @media (max-width: 767px) {
-                padding: 0 16px;
-            }
-        }
+        margin-bottom: 2rem;
+        height: 72px;
+        padding: 8px 64px;
+
+        background: linear-gradient(#1d104b, transparent);
+
+        animation: all 0.5s ease;
 
         &--day {
-            background-color: #2885dd;
+            background: linear-gradient(#89c6ff, #2885dd, transparent);
         }
 
-        &--night {
-            background-color: #111128;
+        @media (max-width: 767px) {
+            padding: 16px;
+
+            h1 {
+                display: none;
+            }
         }
 
-        &__footer {
-            padding-top: 16px;
-            text-align: center;
+        h1 {
+            font-weight: 500;
+            font-size: 24px;
+            color: #f8f8f8;
         }
     }
 
-    @keyframes fadein {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
+    &__menu-button {
+        margin-left: 16px;
+
+        height: 40px;
+        width: 40px;
+        border-radius: 16px;
+        border: none;
+    }
+
+    &__container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: start;
+        width: 100%;
+
+        @media (max-width: 767px) {
+            padding: 0 16px;
         }
     }
+
+    &--day {
+        background-color: #2885dd;
+    }
+
+    &--night {
+        background-color: #111128;
+    }
+
+    &__footer {
+        padding-top: 16px;
+        text-align: center;
+    }
+}
+
+@keyframes fadein {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
 </style>
